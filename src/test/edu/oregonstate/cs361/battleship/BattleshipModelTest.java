b6c3b9077d896f53a5cb8262c001fed8f57b5888
Evan Milton <vanthemilton@gmail.com>
2017-02-01 16:16:08 -0800

    removed .idea

--- src/test/edu/oregonstate/cs361/battleship/BattleshipModelTest.java
+++ src/test/edu/oregonstate/cs361/battleship/BattleshipModelTest.java
@@ -1,30 +1,35 @@
 package edu.oregonstate.cs361.battleship;
 
-import com.google.gson.Gson;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import spark.Spark;
-import spark.utils.IOUtils;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static spark.Spark.awaitInitialization;
 
 class BattleshipModelTest {
 
     @Test
     public void testTest() {
-        Point pnt1 = new Point(1,1);
-        Point pnt2 = new Point(5, 1);
 
-        BattleshipModel ship = new BattleshipModel(new String("aircraftCarrier"), pnt1, pnt2);
-        assertEquals(1, ship.test());
+        BattleshipModel test = new BattleshipModel();
+
+        // Test computer ships
+
+        //      Aircraft Carrier
+//        assertEquals(-1,test.getComputerAircraftCarrier().getStart().getAcross());
+//        assertEquals(-1,test.getComputerAircraftCarrier().getStart().getDown());
+//
+//        //      Battleship
+//        assertEquals(-1,test.getComputerBattleship().getStart().getAcross());
+//        assertEquals(-1,test.getComputerBattleship().getStart().getDown());
+//
+//        //      Cruiser
+//        assertEquals(-1,test.getComputerCruiser().getStart().getAcross());
+//        assertEquals(-1,test.getComputerCruiser().getStart().getDown());
+//
+//        //      Destroyer
+//        assertEquals(-1,test.getComputerDestroyer().getStart().getAcross());
+//        assertEquals(-1,test.getComputerDestroyer().getStart().getDown());
+//
+//        //      Submarine
+//        assertEquals(-1,test.getComputerSubmarine().getStart().getAcross());
+//        assertEquals(-1,test.getComputerSubmarine().getStart().getDown());
     }
 
 }
